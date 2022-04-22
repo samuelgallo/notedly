af6f2a60c890ca8ba94c60fcc686f3b419a36a2a
Samuel Gallo <samuel@samuelgallo.com>
2022-04-21 23:33:44 -0400

    jsonwebtoken user signup and signin

--- api/src/resolvers/mutation.js
+++ api/src/resolvers/mutation.js
@@ -1,3 +1,12 @@
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {
+  AuthenticationError,
+  ForbiddenError
+} = require('apollo-server-express');
+const gravatar = require('../util/gravatar');
+
 const models = require('../models');
 
 module.exports = {
@@ -30,5 +39,54 @@ module.exports = {
         new: true
       }
     );
+  },
+  signUp: async (parent, { username, email, password }, { models }) => {
+    // normalize the email address
+    email = email.trim().toLowerCase();
+
+    // crypt the password
+    const hashed = await bcrypt.hash(password, 10);
+
+    const avatar = await gravatar(email);
+
+    try {
+      // validation by email and username only one is allowed
+      const user = await models.User.create({
+        username,
+        email,
+        avatar,
+        password: hashed
+      });
+
+      return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    } catch (err) {
+      console.log(err);
+
+      throw new Error('Error creating account');
+    }
+  },
+  signIn: async (parent, { username, email, password }, { models }) => {
+    if (email) {
+      email = email.trim().toLowerCase();
+    }
+
+    // fint by email or username
+    const user = await models.User.findOne({
+      $or: [{ email }, { username }]
+    });
+
+    // if no user is found throw an authentication error
+    if (!user) {
+      throw new AuthenticationError('Error signing in');
+    }
+
+    // if the password dont match throw an authentication error
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new AuthenticationError('Error signing in (password)');
+    }
+
+    // create and return the json web token
+    return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
   }
 };
--- api/src/schema.js
+++ api/src/schema.js
@@ -10,14 +10,27 @@ module.exports = gql`
     createdAt: DateTime!
     updatedAt: DateTime!
   }
+
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    avatar: String
+    notes: [Note!]!
+  }
+
   type Query {
     hello: String!
     notes: [Note!]!
     note(id: ID!): Note!
   }
+
   type Mutation {
     newNote(content: String!, author: String!): Note!
     updateNote(id: ID!, content: String!, author: String): Note!
     deleteNote(id: ID!): Boolean!
+
+    signUp(username: String!, email: String!, password: String!): String!
+    signIn(username: String, email: String, password: String!): String!
   }
 `;
